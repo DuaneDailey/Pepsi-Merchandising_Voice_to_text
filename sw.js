91dea54b3161e795d2331295a01136fc04156df2
Duane Dailey <duanedailey@gmail.com>
2026-06-02 21:06:52 -0400

    Refactor service worker for cache management

--- sw.js
+++ sw.js
@@ -1,17 +1,11 @@
-const CACHE_NAME = "pepsico-fill-v3";
-const ASSETS = ["./", "./index.html", "./manifest.json"];
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
+const CACHE_NAME="pepsico-v5";
+const ASSETS=["./","./index.html","./manifest.json"];
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.map(k => (k === CACHE_NAME ? null : caches.delete(k)))))
-  );
+self.addEventListener("activate",e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));
 });
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((resp) => resp || fetch(event.request).catch(() => null))
-  );
+self.addEventListener("fetch",e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>null)));
 });
