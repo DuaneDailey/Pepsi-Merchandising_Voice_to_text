d6125258165425cbb1113eceba7175c7e914ff4a
Duane Dailey <duanedailey@gmail.com>
2026-06-02 21:21:08 -0400

    Upgrade service worker to version 6

--- sw.js
+++ sw.js
@@ -1,11 +1,24 @@
-const CACHE_NAME="pepsico-v5";
+const CACHE_NAME="pepsico-v6";
 const ASSETS=["./","./index.html","./manifest.json"];
 self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(c=>c.addAll(ASSETS))
+      .then(()=>self.skipWaiting())
+  );
 });
 self.addEventListener("activate",e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));
+  e.waitUntil(
+    caches.keys().then(keys=>
+      Promise.all(keys.map(k=>
+        k!==CACHE_NAME?caches.delete(k):null
+      ))
+    )
+  );
 });
 self.addEventListener("fetch",e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>null)));
+  e.respondWith(
+    caches.match(e.request)
+      .then(r=>r||fetch(e.request).catch(()=>null))
+  );
 });
